93a2befa211b099fc9e1c63464e1ecbdd9aae0dd
Aleksandr <meynspain@gmail.com>
2025-04-28 16:49:16 +0900

    Добавил простенькую анимацию при добавлении в избранное.

--- lib/features/characters_list/view/widgets/character_card.dart
+++ lib/features/characters_list/view/widgets/character_card.dart
@@ -16,8 +16,31 @@ class CharacterCard extends StatefulWidget {
   State<CharacterCard> createState() => _CharacterCardState();
 }
 
-class _CharacterCardState extends State<CharacterCard> {
+class _CharacterCardState extends State<CharacterCard>
+    with SingleTickerProviderStateMixin {
   bool _isExpanded = false;
+
+  late final AnimationController _animationController;
+  late final Animation<double> _scaleAnimation;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+
+    _scaleAnimation = Tween(begin: 1.0, end: 1.5).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -38,13 +61,26 @@ class _CharacterCardState extends State<CharacterCard> {
 
             title: Text('${character.id} - ${character.name}'),
             subtitle: Text('${character.species} - ${character.status}'),
-            trailing: IconButton(
-              onPressed: widget.onFavoriteTap,
-              icon:
-                  character.isFavorite
-                      ? Icon(Icons.star)
-                      : Icon(Icons.star_border),
-              color: character.isFavorite ? Colors.amber : null,
+            trailing: AnimatedBuilder(
+              animation: _scaleAnimation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _scaleAnimation.value,
+                  child: IconButton(
+                    onPressed: () {
+                      _animationController.forward().then((_) =>
+                          _animationController.reverse());
+                      widget.onFavoriteTap();
+                    },
+                    icon:
+                    character.isFavorite
+                        ? Icon(Icons.star)
+                        : Icon(Icons.star_border),
+                    color: character.isFavorite ? Colors.amber : null,
+                  ),
+                );
+              },
+
             ),
           ),
           AnimatedCrossFade(
@@ -53,28 +89,30 @@ class _CharacterCardState extends State<CharacterCard> {
             secondCurve: Curves.easeIn,
             firstChild: SizedBox(),
             crossFadeState:
-                _isExpanded
-                    ? CrossFadeState.showSecond
-                    : CrossFadeState.showFirst,
+            _isExpanded
+                ? CrossFadeState.showSecond
+                : CrossFadeState.showFirst,
             secondChild:
-                _isExpanded
-                    ? Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 8,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Type - ${character.type.isNotEmpty ? character.type : 'Unknown'}',
-                          ),
-                          Text('Gender - ${character.gender}'),
-                          Text('Origin - ${character.origin}'),
-                        ],
-                      ),
-                    )
-                    : SizedBox(),
+            _isExpanded
+                ? Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Type - ${character.type.isNotEmpty
+                        ? character.type
+                        : 'Unknown'}',
+                  ),
+                  Text('Gender - ${character.gender}'),
+                  Text('Origin - ${character.origin}'),
+                ],
+              ),
+            )
+                : SizedBox(),
           ),
           InkWell(
             onTap: () {
